b29649627f6dd6d6c5c997edf7061d8b84573833
abdulazim <abdorabie14@gmail.com>
2024-08-23 06:02:41 +0300

    creating-logo-component-and-refactor-logos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthorsComponent } from './components/authors/authors.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RegisterComponent } from './components/register/register.component';
     AuthorsComponent,
     FooterComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
